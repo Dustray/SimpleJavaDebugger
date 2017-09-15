050099ee4ac6a176380afc9c24f03c6f6a8f27fe
Dustray <yinxiyue1995@outlook.com>
2017-09-15 19:31:50 +0800

    beta1

---
+++ SimpleJavaDebugger/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace SimpleJavaDebugger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.saveFile = new System.Windows.Forms.Button();
+            this.compileAndRun = new System.Windows.Forms.Button();
+            this.run = new System.Windows.Forms.Button();
+            this.compile = new System.Windows.Forms.Button();
+            this.saveJavaFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.saveStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.debugStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.groupBox1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // inputBox
+            // 
+            this.inputBox.AcceptsTab = true;
+            this.inputBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.inputBox.Font = new System.Drawing.Font("宋体", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.inputBox.Location = new System.Drawing.Point(10, 27);
+            this.inputBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.inputBox.Multiline = true;
+            this.inputBox.Name = "inputBox";
+            this.inputBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.inputBox.Size = new System.Drawing.Size(424, 457);
+            this.inputBox.TabIndex = 0;
+            // 
+            // outputBox
+            // 
+            this.outputBox.AcceptsTab = true;
+            this.outputBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.outputBox.Location = new System.Drawing.Point(437, 171);
+            this.outputBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.outputBox.Multiline = true;
+            this.outputBox.Name = "outputBox";
+            this.outputBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.outputBox.Size = new System.Drawing.Size(313, 313);
+            this.outputBox.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.saveFile);
+            this.groupBox1.Controls.Add(this.compileAndRun);
+            this.groupBox1.Controls.Add(this.run);
+            this.groupBox1.Controls.Add(this.compile);
+            this.groupBox1.Location = new System.Drawing.Point(438, 27);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(311, 139);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "控制";
+            // 
+            // saveFile
+            // 
+            this.saveFile.Location = new System.Drawing.Point(232, 28);
+            this.saveFile.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.saveFile.Name = "saveFile";
+            this.saveFile.Size = new System.Drawing.Size(75, 24);
+            this.saveFile.TabIndex = 3;
+            this.saveFile.Text = "保存文件";
+            this.saveFile.UseVisualStyleBackColor = true;
+            this.saveFile.Click += new System.EventHandler(this.saveFile_Click);
+            // 
+            // compileAndRun
+            // 
+            this.compileAndRun.Location = new System.Drawing.Point(5, 86);
+            this.compileAndRun.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.compileAndRun.Name = "compileAndRun";
+            this.compileAndRun.Size = new System.Drawing.Size(75, 24);
+            this.compileAndRun.TabIndex = 2;
+            this.compileAndRun.Text = "编译并运行";
+            this.compileAndRun.UseVisualStyleBackColor = true;
+            this.compileAndRun.Click += new System.EventHandler(this.compileAndRun_Click);
+            // 
+            // run
+            // 
+            this.run.Enabled = false;
+            this.run.Location = new System.Drawing.Point(5, 57);
+            this.run.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.run.Name = "run";
+            this.run.Size = new System.Drawing.Size(75, 24);
+            this.run.TabIndex = 1;
+            this.run.Text = "运行";
+            this.run.UseVisualStyleBackColor = true;
+            this.run.Click += new System.EventHandler(this.run_Click);
+            // 
+            // compile
+            // 
+            this.compile.Location = new System.Drawing.Point(5, 28);
+            this.compile.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.compile.Name = "compile";
+            this.compile.Size = new System.Drawing.Size(75, 24);
+            this.compile.TabIndex = 0;
+            this.compile.Text = "编译";
+            this.compile.UseVisualStyleBackColor = true;
+            this.compile.Click += new System.EventHandler(this.compile_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveStatusLabel,
+            this.debugStatusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 486);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(758, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // saveStatusLabel
+            // 
+            this.saveStatusLabel.Name = "saveStatusLabel";
+            this.saveStatusLabel.Size = new System.Drawing.Size(68, 17);
+            this.saveStatusLabel.Text = "文件未保存";
+            // 
+            // debugStatusLabel
+            // 
+            this.debugStatusLabel.Name = "debugStatusLabel";
+            this.debugStatusLabel.Size = new System.Drawing.Size(68, 17);
+            this.debugStatusLabel.Text = "未开始调试";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.AllowDrop = true;
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(758, 25);
+            this.toolStrip1.TabIndex = 4;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(758, 508);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.inputBox);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.groupBox1.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.TextBox outputBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button saveFile;
+        private System.Windows.Forms.Button compileAndRun;
+        private System.Windows.Forms.Button run;
+        private System.Windows.Forms.Button compile;
+        private System.Windows.Forms.SaveFileDialog saveJavaFileDialog;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel saveStatusLabel;
+        private System.Windows.Forms.ToolStripStatusLabel debugStatusLabel;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+    }
+}
+
---
+++ SimpleJavaDebugger/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleJavaDebugger
+{
+    public partial class Form1 : Form
+    {
+        public static string filePathWithName = "";
+        public static string filePathWithoutName = "";
+        public static string fileName = "";
+        public static string className = "";
+        public static bool isCompiled = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void saveFile_Click(object sender, EventArgs e)
+        {
+            saveJavaCode();
+        }
+        //编译
+        private void compile_Click(object sender, EventArgs e)
+        {
+            compileJava();
+        }
+
+        private void run_Click(object sender, EventArgs e)
+        {
+            runClass();
+        }
+
+        private void compileAndRun_Click(object sender, EventArgs e)
+        {
+            if (compileJava() == 1)
+                runClass();
+        }
+
+        public bool saveJavaFile()
+        {
+            // "保存为"对话框
+            SaveFileDialog dialog = saveJavaFileDialog;
+            dialog.Filter = "java文件|*.java";
+            // 显示对话框
+            saveStatusLabel.Text = "正在保存...";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                // 路径带文件名
+                filePathWithName = dialog.FileName;
+                //获取文件名，不带路径  
+                fileName = filePathWithName.Substring(filePathWithName.LastIndexOf("\\") + 1);
+                //获取文件路径，不带文件名 
+                filePathWithoutName = filePathWithName.Substring(0, filePathWithName.LastIndexOf("\\"));
+                className = fileName.Replace(".java", "");
+                saveStatusLabel.Text = "文件位置:" + filePathWithName;
+                return true;
+            }
+            else// if(dialog.ShowDialog() == DialogResult.Cancel)
+            {
+                saveStatusLabel.Text = "文件未保存";
+                return false;
+            }
+        }
+        public bool saveJavaCode()
+        {
+            if (filePathWithName == "")
+            {
+                if (!saveJavaFile())
+                    return false;
+            }
+
+            // 创建文件，准备写入
+            FileStream fs = File.Open(filePathWithName,
+                    FileMode.Create,
+                    FileAccess.Write);
+            StreamWriter wr = new StreamWriter(fs, Encoding.Default);
+
+            // 逐行将textBox1的内容写入到文件中
+            foreach (string line in inputBox.Lines)
+            {
+                string str = line.Replace("\n", "");
+                wr.WriteLine(str);
+
+            }
+
+            // 关闭文件
+            wr.Flush();
+            wr.Close();
+            fs.Close();
+            return true;
+
+        }
+        public void openJavaCode()
+        {
+            //if (filePathWithName == "") openJavaFile();
+            // 创建文件，准备写入
+            using (FileStream fs = File.Open(filePathWithName, FileMode.Open))
+            {
+                StreamReader sr = new StreamReader(fs, Encoding.Default);
+                string myStr = sr.ReadToEnd();
+                inputBox.Text = myStr;
+            };
+        }
+        public int compileJava()
+        {
+            debugStatusLabel.Text = "正在编译...";
+            debugStatusLabel.ForeColor = Color.Orange;
+            if (!saveJavaCode())
+                return -1;
+            string sPath = Environment.GetEnvironmentVariable("JAVA_HOME");
+
+            string path = sPath + @"\bin\javac";
+            string fileNames = path;
+            Process p = new Process();
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardError = true;
+            p.StartInfo.RedirectStandardInput = true;
+            p.StartInfo.FileName = fileNames;
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.Arguments = filePathWithName;//参数以空格分隔，如果某个参数为空，可以传入””
+            StreamWriter s = p.StandardInput;
+            s.WriteLine("s");
+//                p.StandardInput.WriteLine("exit");
+            p.StandardInput.AutoFlush = true;
+            p.Start();
+
+          
+            p.WaitForExit();
+            //此处可以返回一个字符串，此例是返回压缩成功之后的一个文件路径
+            
+            string output = p.StandardOutput.ReadToEnd();
+            string error = p.StandardError.ReadToEnd();
+
+            
+            if (output == "" && error == "")
+            {
+                isCompiled = true;
+                run.Enabled = true;
+                outputBox.Text = "";
+                debugStatusLabel.Text = "编译成功！";
+                debugStatusLabel.ForeColor = Color.Green;
+                return 1;
+            }
+            else
+            {
+                outputBox.Text = error;
+                debugStatusLabel.Text = "编译失败！";
+                debugStatusLabel.ForeColor = Color.Red;
+                return 0;
+            }
+        }
+
+        public void runClass()
+        {
+            debugStatusLabel.Text = "正在运行...";
+            debugStatusLabel.ForeColor = Color.Orange;
+            // if (filePathWithName == "") saveJavaFile();
+            string sPath = Environment.GetEnvironmentVariable("JAVA_HOME");
+
+            string path = sPath + @"\bin\java";
+            string fileNames = path;
+            Process p = new Process();
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardError = true;
+            p.StartInfo.FileName = fileNames;
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.Arguments = "-classpath " + filePathWithoutName + " " + className;//参数以空格分隔，如果某个参数为空，可以传入””
+            p.Start();
+            p.WaitForExit();
+            //此处可以返回一个字符串，此例是返回压缩成功之后的一个文件路径
+            string output = p.StandardOutput.ReadToEnd();
+            string error = p.StandardError.ReadToEnd();
+            if (output == "" && error == "")
+            {
+                isCompiled = true;
+                run.Enabled = true;
+                outputBox.Text = "程序运行无返回结果";
+
+                debugStatusLabel.Text = "运行成功！";
+                debugStatusLabel.ForeColor = Color.Green;
+            }
+            else if (error != "")
+            {
+                outputBox.Text = "ERROR:" + error + "\r\n" + output.Replace("\n", "\r\n");
+
+                debugStatusLabel.Text = "运行失败！";
+                debugStatusLabel.ForeColor = Color.Red;
+            }
+            else
+            {
+                outputBox.Text = output.Replace("\n", "\r\n"); ;
+
+                debugStatusLabel.Text = "运行成功！";
+                debugStatusLabel.ForeColor = Color.Green;
+            }
+        }
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.All;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            // 路径带文件名
+            filePathWithName = ((string[])e.Data.GetData(DataFormats.FileDrop, false))[0];
+            //获取文件名，不带路径
+            fileName = filePathWithName.Substring(filePathWithName.LastIndexOf("\\") + 1);
+            //获取文件路径，不带文件名 
+            filePathWithoutName = filePathWithName.Substring(0, filePathWithName.LastIndexOf("\\"));
+            className = fileName.Replace(".java", "");
+            saveStatusLabel.Text = "文件位置:" + filePathWithName;
+            openJavaCode();
+        }
+    }
+}
